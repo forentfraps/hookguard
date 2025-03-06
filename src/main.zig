0a84f08fc5d2a50b3fce717195136b61383b3682
forentfraps <vacpox.com>
2025-03-06 20:25:02 +0000

    init commit

---
+++ src/main.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const syscall_lib = @import("syscall.zig");
+const winc = @import("Windows.h.zig");
+const win = std.os.windows;
+
+const syscall = syscall_lib.syscall;
+const W = std.unicode.utf8ToUtf16LeStringLiteral;
+
+pub fn main() !void {
+    const ntdll = win.kernel32.GetModuleHandleW(W("ntdll.dll")).?;
+    const ntqsi: [*]u8 = @ptrCast(win.kernel32.GetProcAddress(ntdll, "NtQuerySystemInformation").?);
+
+    var s = try syscall.fetch(ntqsi);
+    var storage: u64 = 0;
+    var buf: [20]u8 = undefined;
+    var result = s.call(.{ 0, @intFromPtr(&buf), 20, @intFromPtr(&storage) });
+    std.debug.print("Syscall returned: {x} desired size is: {d}\n", .{ result, storage });
+    const alloc = std.heap.page_allocator;
+    var new_buf = std.ArrayList(u8).init(alloc);
+    try new_buf.resize(storage);
+    result = s.call(.{ 0, @intFromPtr(&new_buf), storage, @intFromPtr(&storage) });
+
+    std.debug.print("Syscall returned: {x}\n", .{result});
+}
