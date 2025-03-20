a7167688c58f737f7c60db70cdd58633aab7d218
The One The Master <vacpox@gmail.com>
2025-03-20 20:06:33 +0000

    KISS, sadly my metaprogramming was for nothing

--- src/state_manager.zig
+++ src/state_manager.zig
@@ -14,9 +14,8 @@ const state_manager_error = error{
     ArgMismatch,
 };
 
-pub fn CallBuffer(func_ptr: anytype, arg: anytype) type {
+pub fn CallBuffer(func_ptr: anytype) type {
     const func_type = @TypeOf(func_ptr);
-    const arg_type = @TypeOf(arg);
     const funcTypeInfo = @typeInfo(func_type);
     var fnInfo: std.builtin.Type.Fn = undefined;
     switch (funcTypeInfo) {
@@ -33,67 +32,16 @@ pub fn CallBuffer(func_ptr: anytype, arg: anytype) type {
 
     const fnArgs = fnInfo.params;
 
-    const argTypeInfo = @typeInfo(arg_type);
-    var arg_len: comptime_int = 0;
-    _ = switch (argTypeInfo) {
-        .@"struct" => |structInfo| {
-            if (structInfo.fields.len != fnArgs.len) {
-                @compileError("Argument mismatch");
-            }
-            // Compare each field's type with the corresponding function parameter.
-            for (fnArgs, 0..) |fn_arg, i| {
-                const field = structInfo.fields[i];
-                // @compileLog(field.type);
-                // @compileLog(fn_arg.type.?);
-                if (field.type != fn_arg.type.?) {
-                    @compileError("Argument mismatch");
-                }
-                arg_len += 1;
-                // Optional: If you want to match names as well,
-                // you can compare `field.name` with `fn_arg.name`.
-            }
-        },
-        else => @compileError("Argument mismatch"),
-    };
     // @compileLog(arg_type);
 
-    // If we got here, the argument matches.
     return struct {
         registers: [9]usize = undefined,
         func_ptr: func_type = func_ptr,
         arg_count: usize = fnArgs.len,
-        // arg_sizes: [fnArgs.len]u8 = arg_size_closure: {
-        //     var local_arg_sizes: [fnArgs.len]u8 = undefined;
-        //     for (fnArgs, 0..) |fn_arg, i| {
-        //         local_arg_sizes[i] = @truncate(@sizeOf(fn_arg.type.?));
-        //     }
-        //
-        //     break :arg_size_closure local_arg_sizes;
-        // },
-        arg: [fnArgs.len]usize = arg_cast_closure: {
-            var local_arg_cast: [fnArgs.len]usize = undefined;
-            for (fnArgs, 0..) |fn_arg, i| {
-                switch (fn_arg.type.?) {
-                    std.builtin.Type.Pointer => {
-                        local_arg_cast[i] = @intFromPtr(arg[i]);
-                    },
-                    else => {
-                        local_arg_cast[i] = @intCast(arg[i]);
-                    },
-                    // else => {
-                    //     @compileLog(fn_arg.type.?);
-                    //     @compileError("UNSUPPORTED TYPE");
-                    // },
-                }
-            }
-            break :arg_cast_closure local_arg_cast;
-        },
-        // TODO add support for xmm+ registers
+        arg: [fnArgs.len]usize = undefined,
 
-        pub fn change_arg(self: *@This(), new_arg: arg_type) void {
+        pub fn call(self: *@This(), new_arg: [fnArgs.len]usize) usize {
             self.arg = new_arg;
-        }
-        pub fn call(self: *@This()) usize {
             warden_lib.global_warden.?.register_call(self) catch {
                 return 0;
             };
